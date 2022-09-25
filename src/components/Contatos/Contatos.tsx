98c050d449b1928e4452f06c709d610a66426f98
henrique <henrique.almeida231@hotmail.com>
2022-09-24 23:17:17 -0300

    add email

--- src/components/Contatos/Contatos.tsx
+++ src/components/Contatos/Contatos.tsx
@@ -3,15 +3,20 @@ import './contatos.scss'
 export const Contatos = ()=>{
     return (
         <div className='contatos' id='contatos'>
-            <div className='GitHub'>
-                <a href='https://github.com/HenriqueeAl' target='_blank'>
-                    <img src='github.png'></img>
-                </a>
+            <div className='row'>
+                <div className='GitHub'>
+                    <a href='https://github.com/HenriqueeAl' target='_blank'>
+                        <img src='github.png'></img>
+                    </a>
+                </div>
+                <div className='LinkedIn'>
+                    <a href='https://www.linkedin.com/in/henrique-almeida-de-oliveira/' target='_blank'>
+                        <img src='linkedin.png'></img>
+                    </a>
+                </div>
             </div>
-            <div className='LinkedIn'>
-                <a href='https://www.linkedin.com/in/henrique-almeida-de-oliveira/' target='_blank'>
-                    <img src='linkedin.png'></img>
-                </a>
+            <div className='email'>
+                  <p>henrique.almeida231@hotmail.com</p>  
             </div>
         </div>
     )
