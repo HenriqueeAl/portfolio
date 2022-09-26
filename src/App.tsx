1316f693cbe95d36c9c5905a479b997b8c1f4bdb
henrique <henrique.almeida231@hotmail.com>
2022-09-26 09:38:21 -0300

    menu mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import { Habilidades } from './components/Habilidades/Habilidades'
 import { Projetos } from './components/Projetos/Projetos'
 import { Sobre } from './components/Sobre/Sobre'
 
+const togglemenu = ()=>{
+  document.getElementById('mobile')?.classList.toggle('display')
+  console.log('oi')
+}
+
 function App() {
 
   return (
@@ -18,12 +23,15 @@ function App() {
           </ul>
         </nav>
         <nav className='mobile'>
-          <img src='menu.svg'></img>
-          <ul>
-            <li><a href='#sobre'>Sobre</a></li>
-            <li><a href='#habilidades'>Habilidades</a></li>
-            <li><a href='#projetos'>Projetos</a></li>
-            <li><a href='#contatos'>Contatos</a></li>
+          <img src='menu.svg' onClick={togglemenu}></img>
+          <ul id='mobile'>
+            <div className='close'>
+              <img src='menu.svg' onClick={togglemenu}></img>
+            </div>
+            <li><a href='#sobre' onClick={togglemenu}>Sobre</a></li>
+            <li><a href='#habilidades' onClick={togglemenu}>Habilidades</a></li>
+            <li><a href='#projetos' onClick={togglemenu}>Projetos</a></li>
+            <li><a href='#contatos' onClick={togglemenu}>Contatos</a></li>
           </ul>
         </nav>
       </header>
