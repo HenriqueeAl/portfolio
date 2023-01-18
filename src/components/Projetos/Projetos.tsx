ca2c8aa9360d5d7b9d5e6a04c46f805ca8371611
henrique <henrique.almeida231@hotmail.com>
2023-01-18 11:40:21 -0300

    alterando dados

--- src/components/Projetos/Projetos.tsx
+++ src/components/Projetos/Projetos.tsx
@@ -4,7 +4,8 @@ interface Projeto{
     name: string;
     descricao: string;
     visu: string;
-    repo: string
+    repo: string;
+    tecs: Array<any>;
 }
 
 const Projeto = (props: Projeto)=>{
@@ -14,13 +15,9 @@ const Projeto = (props: Projeto)=>{
             <div className='content'>
                 <p>{props.descricao}</p>
                 <div className='tecs'>
-                    <div className='box-tecs'>HTML</div>
-                    <div className='box-tecs'>SCSS</div>
-                    <div className='box-tecs'>JavaScript</div>
-                    <div className='box-tecs'>TypeScript</div>
-                    <div className='box-tecs'>React</div>
-                    <div className='box-tecs'>Nodejs</div>
-                    <div className='box-tecs'>Prisma</div>
+                    {props.tecs.map((e)=>{
+                        return <div className='box-tecs'>{e}</div>
+                    })}
                 </div>
             </div>
             <div className='links'>
@@ -38,26 +35,31 @@ export const Projetos = ()=>{
             <div className='lista-projetos'>
                 <Projeto name='Todo list / Lista de Afazeres'
                 descricao='Meu primeiro projeto full-stack com ele eu aprendi bastante sobre NodeJs e banco de dados.'
+                tecs={['HTML', 'SCSS', 'TypeScript', 'React', 'Nodejs', 'Prisma']}
                 visu='https://todolist-crud.vercel.app/'
                 repo='https://github.com/HenriqueeAl/Todolist-crud'
                 />
                 <Projeto name='Blizzard'
                 descricao='Um desafio da plataforma brchallenges feito com ReactJs, TypeScript e SASS.'
+                tecs={['HTML', 'SCSS', 'TypeScript', 'React', 'Axios']}
                 visu='https://blizzard-iota.vercel.app/'
                 repo='https://github.com/HenriqueeAl/blizzard'
                 />
                 <Projeto name='Pizza Time' 
                 descricao='Landing Page de pizzas utilizando Nexts com TypeScript e SASS.'
+                tecs={['HTML', 'SCSS', 'TypeScript', 'React']}
                 visu='https://pizza-time-five.vercel.app/'
                 repo='https://github.com/HenriqueeAl/Pizza-Time'
                 />
                 <Projeto name='Burger Empire'
                 descricao='Um desafio da plataforma brchallenges feito com HTML, Sass, ReactJs e TypeScript.'
+                tecs={['HTML', 'SCSS', 'TypeScript', 'React']}
                 visu='https://empire-burger-brchallenges.vercel.app/'
                 repo='https://github.com/HenriqueeAl/Empire-Burger---BRChallenges'
                 />
                 <Projeto name='Space'
-                descricao='Um desafio da plataforma brchallenges feito com HTML, Sass, ReactJs e TypeScript.'
+                descricao='Uma landing page sobre o espaço.'
+                tecs={['HTML', 'SCSS', 'TypeScript', 'React']}
                 visu='https://space-three-rose.vercel.app/home'
                 repo='https://github.com/HenriqueeAl/Space'
                 />
